e6a7373839044599b7e40f52c5edccbf9724aca1
Israel <israeleadetunji@gmail.com>
2023-02-10 22:21:16 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  url: string;
+  description: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'udacity-angular-project';
+  products: any = [];
+
+  constructor(private _apiService: HttpService) {}
+
+  ngOnInit() {
+    this._apiService.getData().subscribe((res) => {
+      // console.log(res.);
+      this.products = res;
+    });
+  }
 }
