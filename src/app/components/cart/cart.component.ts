b8af6390b88b50a0df4cfaf2d87273b64c8c4ec4
Israel <israeleadetunji@gmail.com>
2023-02-14 15:18:48 +0100

    implement cart ui

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/utils/types';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: Product[] = [];
+  total = 0;
+  card = '';
+  name = '';
+  address = '';
+  disabled =
+    this.card.length < 3 && this.name.length < 3 && this.address.length < 3;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cart = this.cartService.getCart();
+    this.total = this.cartService.getTotalCost();
+    console.log(this.disabled);
+  }
+
+  updateQuantity(id: number, quantity: number | undefined) {
+    console.log(`ID: ${id} Quantity: ${quantity}`);
+    this.cartService.updateCartQuantity(id, quantity);
+    this.total = this.cartService.getTotalCost();
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,15 +1,59 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../app.component';
+import { Product } from '../../utils/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private cartItems: Product[] = [];
+  private cartItems: Product[] = [
+    {
+      description: 'You can read it!',
+      id: 1,
+      name: 'Book',
+      price: 9.99,
+      quantity: 3,
+      url: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+    },
+  ];
+  private totalCost = 0;
+  fullName: string = '';
+  address: string = '';
+  creditCard: string = '';
+
   constructor() {}
 
   addToCart(item: Product) {
     this.cartItems.push(item);
     console.log(this.cartItems);
+  }
+
+  getCart() {
+    return this.cartItems;
+  }
+
+  getTotalCost() {
+    this.cartItems.forEach((item) => {
+      if (item.quantity) {
+        this.totalCost += item.price * item.quantity;
+      }
+    });
+    return this.totalCost;
+  }
+
+  updateCartQuantity(id: number, quantity: number | undefined) {
+    this.cartItems.forEach((item) => {
+      if (item.id === id) {
+        // update total cost
+        if (item.quantity) {
+          this.totalCost = this.totalCost - item.price * item.quantity;
+        }
+
+        console.log(this.totalCost, item.quantity, quantity);
+
+        item.quantity = quantity;
+        if (quantity) this.totalCost += item.price * quantity;
+        console.log(this.totalCost);
+      }
+    });
   }
 }
